6c466a73042f80136ba7c8ff65cf9e3e8fef7593
yolken <yolken>
2016-11-19 23:02:46 -0800

    First draft of Midterm complete

--- Midterm/frequencylockedloop.m
+++ Midterm/frequencylockedloop.m
@@ -29,13 +29,13 @@ x1=filter(h,1,x1);
 % Plot real part of first 100 symbols
 figure
 subplot(2,1,1)
-plot(0:99, real(x1(1:8:8*100)),'ro')
+plot(0:99, real(x1(1:8:8*100)),'k')
 title('Real part of first 100 symbols')
 ylim([-1.5 1.5])
 
 % Plot imaginary part of first 100 symbols
 subplot(2,1,2)
-plot(0:99, imag(x1(1:8:8*100)),'ro')
+plot(0:99, imag(x1(1:8:8*100)),'k')
 title('Imaginary part of first 100 symbols')
 ylim([-1.5 1.5])
 
@@ -63,8 +63,10 @@ title('Constellation diagram at modulator output')
 % Task c
 
 % Generate band edge filter
-[h2, be_pos, be_bb] = band_edge_harris(4,0.5,6);
+[h2, be_pos, be_bb] = band_edge_harris_2(4,0.5,6);
+be_pos = be_pos/sum(h2);
 be_neg = conj(be_pos);
+
 
 % Plot impulse response of downsampled shaping filter
 figure
@@ -83,7 +85,7 @@ title('Impulse response of positive band-edge filter')
 subplot(3,1,3)
 plot(linspace(-0.5,0.5,1024)*4,fftshift(abs(fft(be_neg,1024))),'b')
 hold on
-plot(linspace(-0.5,0.5,1024)*4,fftshift(abs(fft(be_bb,1024))),'r')
+plot(linspace(-0.5,0.5,1024)*4,fftshift(abs(fft(h2/sum(h2),1024))),'r')
 hold on
 plot(linspace(-0.5,0.5,1024)*4,fftshift(abs(fft(be_pos,1024))),'b')
 title('Frequency response of band-edge filters')
@@ -105,7 +107,10 @@ ww=10*ww/sum(ww);
 % Plot spectrum of modulated signal passed through matched filter
 figure
 subplot(2,1,1)
-plot(linspace(-0.5,0.5,2048)*4,fftshift(20*log10(abs(fft(y1(1:2048).*ww)))),'k')
+plot(linspace(-0.5,0.5,1024)*4,fftshift(20*log10(abs(fft(h2/sum(h2),1024)))),'r')
+hold on
+plot(linspace(-0.5,0.5,2048)*4,fftshift(20*log10(abs(fft(y1(1:2048).*ww)))),'b')
+axis([-4/2 4/2 -60 10])
 title('Spectrum of modulated signal passed through matched filter')
 
 % Plot spectrum of modulated signal passed through band-edge filters
@@ -121,8 +126,139 @@ grid on
 axis([-4/2 4/2 -60 10])
 title('Spectrum of modulated signal passed through band-edge filters')
 
-% task e
+% Task e
 
+% Apply spin to the modulated signal
+ff=0.05;
+x3=x2.*exp(j*2*pi*(1:4*N)*ff);
+
+% Pass signal through filters
+y1 = filter(hm,1,x3);
+y2 = filter(be_neg,1,x3);
+y3 = filter(be_pos,1,x3);
+
+% Plot spectrum of modulated signal passed through matched filter
+figure
+subplot(2,1,1)
+plot(linspace(-0.5,0.5,1024)*4,fftshift(20*log10(abs(fft(h2/sum(h2),1024)))),'r')
+hold on
+plot(linspace(-0.5,0.5,2048)*4,fftshift(20*log10(abs(fft(y1(1:2048).*ww)))),'b')
+axis([-4/2 4/2 -60 10])
+title('Spectrum of modulated frequency offset signal passed through matched filter')
+
+% Plot spectrum of modulated signal passed through band-edge filters
+subplot(2,1,2)
+plot(linspace(-0.5,0.5,1024)*4,fftshift(20*log10(abs(fft(be_neg,1024)))),'b')
+hold on
+plot(linspace(-0.5,0.5,2048)*4,fftshift(20*log10(abs(fft(y2(1:2048).*ww)))),'b')
+hold on
+plot(linspace(-0.5,0.5,1024)*4,fftshift(20*log10(abs(fft(be_pos,1024)))),'r')
+hold on
+plot(linspace(-0.5,0.5,2048)*4,fftshift(20*log10(abs(fft(y3(1:2048).*ww)))),'r')
+grid on
+axis([-4/2 4/2 -60 10])
+title('Spectrum of modulated frequency offset signal passed through band-edge filters')
+
+% Task f
+theta_0= 2*pi/500;
+eta=sqrt(2)/2;
+
+k_i_f= (4*theta_0*theta_0)/(1+2*eta*theta_0+theta_0*theta_0);
+k_p_f= (4*eta*theta_0)/(1+2*eta*theta_0+theta_0*theta_0);
+
+reg_f=zeros(1,49);
+int_f=0.0;
+reg_avg=0.0;
+accum_f=0.0;
+
+det_sv=zeros(1,4*N);
+phs_f=zeros(1,4*N);
+sm_f_sv=zeros(1,4*N);
+% Frequency lock loop
+for nn=1:4*N
+    x4(nn)=x3(nn)*exp(-j*2*pi*accum_f);  
+    reg_f=[x4(nn) reg_f(1:48)];
+    be_1=reg_f*be_pos.';
+    be_2=reg_f*be_neg.';
+
+    be_1_sv(nn)=be_1;
+    be_2_sv(nn)=be_2;
+
+    det=(abs(be_1)^2-abs(be_2)^2);
+    det_sv(nn)=det;
+    reg_avg=0.1*det+0.9*reg_avg;
+    int_f=int_f+k_i_f*reg_avg;
+    sm_f =int_f+k_p_f*reg_avg;
+    sm_f_sv(nn) = sm_f;
+
+    phs_f(nn)=accum_f;
+    accum_f=accum_f+1*sm_f;
+end
+figure
+subplot(2,1,1)
+% Plot frequency response of shaping and band edge filters
+plot(linspace(-0.5,0.5,1024)*4,fftshift(20*log10(abs(fft(h2/sum(h2),1024)))),'b')
+hold on
+plot(linspace(-0.5,0.5,1024)*4,fftshift(20*log10(abs(fft(be_neg,1024)))),'b')
+hold on
+plot(linspace(-0.5,0.5,1024)*4,fftshift(20*log10(abs(fft(be_pos,1024)))),'b')
+hold on
+% Plot input spectrum
+plot(linspace(-0.5,0.5,2048)*4,fftshift(20*log10(abs(fft(x3(1:2048).*ww)))),'r')
+title('Input spectrum and frequency response of shaping and band edge filters')
+subplot(2,1,2)
+% Plot output spectrum
+plot(linspace(-0.5,0.5,2048)*4,fftshift(20*log10(abs(fft(x4(1:2048).*ww)))),'r')
+hold on
+% Plot DDS signal
+plot(linspace(-0.5,0.5,2048)*4,fftshift(20*log10(abs(fft(exp(-j*2*pi*phs_f(1:2048).*ww))))),'b')
+hold on
+% Plot frequency response of shaping filter
+plot(linspace(-0.5,0.5,1024)*4,fftshift(20*log10(abs(fft(h2/sum(h2),1024)))),'m')
+title('Frequency response of shaping filter, FLL output and DDS signal spectrum')
+% Plot phase profile of spinning frequency and phase accumulator
+figure
+subplot(2,1,1)
+plot(1:N*4,(1:4*N)*ff,'b')
+hold on
+plot(1:N*4,phs_f,'r')
+title('Phase profile of spinning frequency and phase accumulator')
+% Plot output of loop filter
+subplot(2,1,2)
+plot(sm_f_sv,'b')
+hold on
+% Plot difference of band edge filters
+plot(det_sv,'r')
+title('Output of loop filter and difference of band edge filters')
+
+%Plot constellation diagrams
+figure
+subplot(1,3,1)
+grid on
+axis('square')
+hold on
+plot(x2(1:4:end),'r.')
+ylim([-1.5 1.5])
+xlim([-1.5 1.5])
+title('Constellation at input of spinner')
+subplot(1,3,2)
+grid on
+axis('square')
+hold on
+plot(x3(1:4:end),'r.')
+ylim([-1.5 1.5])
+xlim([-1.5 1.5])
+title('Constellation at output of spinner')
+subplot(1,3,3)
+grid on
+axis('square')
+hold on
+plot(x4(1:4:2000),'b.')
+hold on
+plot(x4(2001:4:end),'r.')
+ylim([-1.5 1.5])
+xlim([-1.5 1.5])
+title('Constellation at output of FLL')
 
 
 
---
+++ Midterm/timinglockedloop.m
@@ -0,0 +1,180 @@
+clear
+close all
+
+% Task a
+
+% shaping filter
+hhx=rcosine(1,8,'sqrt',0.5,6);   % Shaping Filter
+hh=[hhx zeros(1,7)];            % zero extend for length a multiple of 8             
+hh=hh/max(hh);                  % scale for unity amplitude at modulator
+hh2=reshape(hh,8,13);  
+
+hh_t=rcosine(1,60,'sqrt',0.5,6);                % Matched filter
+hh_t=[hh_t zeros(1,59)];                        % zeros extending
+scl=hh_t(1:30:length(hh_t))*hh(1:4:length(hh))';% scale for unity gain
+hh_t=hh_t/scl;
+hh_t2=reshape(hh_t,30,26);
+
+
+% Plot frequency response for each matched filter
+figure
+subplot(1,2,1)
+for k = 1:30
+    plot(linspace(-0.5,0.5,1024)*2,fftshift(abs(fft(hh_t2(k,:),1024))),'b')
+    hold on
+end
+title('Frequency response of all 30 filters')
+% Plot group delay for each matched filter
+subplot(1,2,2)
+plot(0,0); hold on
+for k=1:30
+[GD,W]=grpdelay(hh_t2(k,:),1,128,'whole');
+plot((-0.5:1/128:0.5-1/128)*2,fftshift(GD))
+grid on
+end
+hold off
+axis([-1 1 10.9 12.1])
+title('Group delay of all 30 filters')
+
+% Task b
+
+% Create modulated data
+N_dat=1000;
+   x0=(floor(2*rand(1,N_dat))-0.5)/0.5+j*(floor(2*rand(1,N_dat))-0.5)/0.5;
+
+% Shape and up-sample input constellation
+reg=zeros(1,13);
+mm=0;               %output clock index
+for nn=1:N_dat
+   reg=[x0(nn) reg(1:12)];
+      for kk=1:8
+          x1(mm+kk)=reg*hh2(kk,:)';
+      end
+   mm=mm+8;
+end
+% Generate timing error   
+x2=x1(3:4:8*N_dat);
+
+% Plot constellation diagram of data passed through each matched filter
+figure
+
+for n = 1:30   
+    subplot(6,5,n)
+    v=filter(hh_t2(n,:),1,x2);
+    grid on
+    axis('square')
+    plot(v(2:2:end),'r.')
+    ylim([-1.5 1.5])
+    xlim([-1.5 1.5])
+    title(['Filter no. ' num2str(n)])
+end
+
+% Task c
+
+% Create polyphase derivative matched filter
+
+dhh_t=conv(hh_t,[1 0 -1]*30);                   % derivative matched filter
+
+dhh_t2=reshape(dhh_t(2:781),30,26);             % 32 path polyphase dMF
+
+% Timing Recovery Loop
+theta_0= 2*pi/200;
+eta=sqrt(2)/2;
+eta=4*eta;
+
+k_i_t= (4*theta_0*theta_0)/(1+2*eta*theta_0+theta_0*theta_0);
+k_p_t= (4*eta*theta_0)/(1+2*eta*theta_0+theta_0*theta_0);
+
+reg_t=zeros(1,26);
+int_t=0.0;
+
+x6 = zeros(1,2*N_dat);
+
+ndx_strt=10;   % Can Change starting index
+
+accum_t=ndx_strt;
+accum_t_sv=zeros(1,N_dat);
+
+% figure(4)
+pp=-1;
+mm=1;                           % output clock at 1-sample per symbol
+for nn=21:2:length(x2)-2
+    reg_t=[x2(nn) reg_t(1:25)];     % new sample in matched filter register
+    pntr=floor(accum_t);            % point to a coefficient set
+    y_t1 =reg_t*hh_t2(pntr,:)';      % MF output time sample
+    dy_t1=reg_t*dhh_t2(pntr,:)';    % dMF output time sample 
+    x6(nn)=y_t1;                    % save MF output sample
+
+    reg_t=[x2(nn+1) reg_t(1:25)];   % new sample in matched filter register
+    y_t2=reg_t*hh_t2(pntr,:)';      % point to a coefficient set
+    x6(nn+1)=y_t2;                   % MF output time sample
+    dy_t2=reg_t*dhh_t2(pntr,:)';    % dMF output time sample 
+
+    det_t=pp*real(y_t2)*real(dy_t2);  % y*y_dot product (timing error)
+    det_t_sv(mm)=det_t;             % save timing error
+    int_t=int_t+k_i_t*det_t;        % Loop filter integrator 
+    sm_t =int_t+k_p_t*det_t;        % loop filter output
+
+    accum_t_sv(mm)=accum_t;         % save timing accumulator content
+    mm=mm+1;                        % increment symbol clock
+    accum_t=accum_t+sm_t;           % update accumulator
+    
+    if accum_t>31                   % test for accumulator overflow
+        accum_t=accum_t-30;
+        pp=-pp;
+    end
+
+    if accum_t<1                    % test for accumulator underflow
+        accum_t=accum_t+30;
+        pp=-pp;
+    end
+end
+
+figure(50)
+subplot(3,1,1)
+plot(accum_t_sv(1:N_dat))
+hold on
+plot(floor(accum_t_sv(1:N_dat)),'r')
+hold off
+grid on
+axis([0 1000 0 30])
+title('Timing Loop Phase Accumulator and Pointer')
+
+
+subplot(3,2,3)
+plot(0,0)
+hold on
+for nn=1000:4:length(x2)-4
+    plot(-1:1/2:1,real(x2(nn:nn+4)))
+end
+hold off
+grid on
+title('Eye Diagram')
+
+subplot(3,2,4)
+plot(x2(2:2:length(x2)),'r.')
+grid on
+axis('square')
+title('Constellation Diagram')
+
+subplot(3,2,5)
+plot(0,0)
+hold on
+for nn=1000:4:length(x6)-4
+    plot(-1:1/2:1,real(x6(nn:nn+4)))
+end
+hold off
+grid on
+title('Eye Diagram')
+
+subplot(3,2,6)
+plot(x6(2:2:200),'.')
+hold on
+plot(x6(1000:2:length(x6)),'r.')
+
+hold off
+grid on
+axis('square')
+title('Constellation Diagram')
+
+
